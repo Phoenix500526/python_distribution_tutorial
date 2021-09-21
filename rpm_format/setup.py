db7b7469fb4388074ec829e78f8a096607720140
Phoenix <phoenix500525@163.com>
2021-09-21 13:17:52 +0800

    为 HelloWorld 添加元信息
    * 目录解释：
      build/ - 构建 rpm 包的工作站
      dist/  - 保存生成的 rpm 包
      HelloWorld.egg-info - 保存了 python package 的基本信息
        PKG-INFO - 包含了一些比较基础的元信息。这些信息也正是当前提交中为 HelloWorld 所添加的基本信息
        SOURCE.txt - 包含了当前目录下的所有需要打包的 .py 文件以及 setup.cfg 文件
    
    * 安装说明：
      对 HelloWorld.rpm 进行安装，实际上就是将 egg-info 中的目录中的文件拷贝到 /usr/lib/python3.6/site-packages/HelloWorld-0.1.0-py3.6.egg-info/ 下
      将 HelloWorld/ 中的文件拷贝到 /usr/lib/python3.6/site-packages/HelloWorld/ 中

--- rpm_format/setup.py
+++ rpm_format/setup.py
@@ -1,4 +1,24 @@
 #-*- coding:utf-8 -*-
 
 from setuptools import setup, find_packages
-setup()
+
+__doc__ = '''
+A Hello world program is usually a program made by computer programmers that are new to a programming languages, 
+or to test if the compiler for this language is working correctly. It will simply put the text Hello, World! on 
+the screen. One way to do the Hello World program is shown below, in the C programming language.
+'''
+
+# zip_safe：用于强制或防 止创建 zip 压缩包。通常你不会想要把包安装为 zip 压缩文件，因为一些工具不支 持压缩文件，而且压缩文件比较难以调试。
+setup(
+    name = "HelloWorld", 
+    version = '0.1.0', 
+    packages = find_packages(),
+    author = 'phoenix', 
+    author_email = 'phoenix@xxx.com',
+    description = 'Hello World python rpm',
+    long_description = __doc__,
+    license = 'GPL',
+    url = 'https://simple.wikipedia.org/wiki/Hello_world_program',
+    platforms = 'x86-64'
+    )
+
