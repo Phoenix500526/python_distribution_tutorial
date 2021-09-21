c655d22cb300ef1770ba907f577eaabd5fcf444a
Phoenix <phoenix500525@163.com>
2021-09-21 13:13:38 +0800

    添加一个 HelloWorld package，并以最简单的方式编译出一个 rpm 包

---
+++ rpm_format/HelloWorld/HelloWorld.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#-*- coding:utf-8 -*-
+
+__author__ = "Phoenix"
+__version__ = '1.0.0'
+
+def helloworld():
+    print("Hello World")
+
+if __name__ == "__main__":
+    helloworld()
